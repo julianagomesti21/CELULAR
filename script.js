bcc43b164d6df0651f84857bd50002d0b4ed967d
julianagomesti21 <julianagomesti@gmail.com>
2026-01-29 21:30:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let contador = 1
+document.querySelector("#bola1").checked = true
+
+
+setInterval(function () {
+        proxima();
+
+}, 4000);
+
+function proxima() {
+        contador = contador + 1
+
+        if (contador > 2) {
+                contador = 1
+        }
+
+        document.querySelector("#bola" + contador).checked = true
+        let valor = 0
+
+        let sanduiche = document.querySelector("#sanduiche")
+        sanduiche.addEventListener("change", comer)
+
+        function comer() {
+                let escolha = sanduiche.value
+                if (escolha == "Sanduiche 1") {
+                        valor = 0
+                        valor = valor + 25
+                }
+        }
+
+        if (escolha == "Sanduiche 2") {
+                valor = 0
+                valor = valor + 30
+        }
+
+        if (escolha == "Sanduiche 3") {
+                valor = 0
+                valor = valor + 35
+        }
+        if (escolha == "Sanduiche 4") {
+                valor = 0
+                valor = valor + 40
+        }
+        if (escolha == "Sanduiche 5") {
+                valor = 0
+                valor = valor + 45
+        }
+
+        if (escolha == "Sanduiche 6") {
+                valor = 0
+                valor = valor + 50
+        }
+
+}
+
+document.querySelector("#resposta").innerHTML = `o valor do seu pedido é ${valor}`
+
+}
